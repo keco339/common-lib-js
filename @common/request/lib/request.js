09e48c9051d3b312979d902f6dc9e166a46f9200
易元平 <keco339@sina.com>
2020-11-02 15:57:28 +0800

    请求失败重试

--- @common/request/lib/request.js
+++ @common/request/lib/request.js
@@ -1,4 +1,5 @@
 const rp = require('request-promise');
+const promiseRetry = require('promise-retry');
 const _ = require('lodash');
 const querystring = require('qs');
 
@@ -20,9 +21,15 @@ class TimeLogger{
 class Request{
     constructor(options){
         this.log = _.get(options,'log') || false;
+        this.retryOptions = {
+            retries: 4,      // 最多重试5次
+            minTimeout: 1000,
+            maxTimeout: 1500,
+        };
     }
-    get(url,qs={},options){
+    get(url,qs={},options,retryOptions){
         options = _.extend({qs: qs, json: true, simple: false, resolveWithFullResponse: true},options);
+        retryOptions = _.extend({}, this.retryOptions, retryOptions);
         let that = this;
 
         if(this.log){
@@ -30,54 +37,98 @@ class Request{
             timeLog.startTime();
         }
 
-        return rp.get(url,options).then(function ({statusCode,body,headers,request}) {
-            if(that.log){
-                timeLog.endTime();
-            }
-            return {statusCode,body,headers,request}
-        })
+        // return rp.get(url,options).then(function ({statusCode,body,headers,request}) {
+        //     if(that.log){
+        //         timeLog.endTime();
+        //     }
+        //     return {statusCode,body,headers,request}
+        // })
+
+        return promiseRetry(function (retry, number) {
+            return rp.get(url,options).then(function ({statusCode,body,headers,request}) {
+                if(that.log){ timeLog.endTime(); }
+                return {statusCode,body,headers,request}
+            }).catch(function (err) {
+                console.log(`${new Date()} retry request ${number} count`);
+                console.error(JSON.stringify(err));
+                retry(err);
+            });
+        }, retryOptions);
     }
-    post(url,data={},options){
+    post(url,data={},options,retryOptions){
         options = _.extend({body: data, json: true, simple: false, resolveWithFullResponse: true},options);
+        retryOptions = _.extend({}, this.retryOptions, retryOptions);
         let that = this;
         if(this.log){
             var timeLog = new TimeLogger(`POST request URL ${url}`);
             timeLog.startTime(); console.log(`body:\n${JSON.stringify(options.body,null,2)}`);
         }
-        return rp.post(url,options).then(function ({statusCode,body,headers,request}) {
-            if(that.log){
-                timeLog.endTime();
-            }
-            return {statusCode,body,headers,request}
-        })
+        // return rp.post(url,options).then(function ({statusCode,body,headers,request}) {
+        //     if(that.log){
+        //         timeLog.endTime();
+        //     }
+        //     return {statusCode,body,headers,request}
+        // })
+
+        return promiseRetry(function (retry, number) {
+            return rp.post(url,options).then(function ({statusCode,body,headers,request}) {
+                if(that.log){ timeLog.endTime();}
+                return {statusCode,body,headers,request}
+            }).catch(function (err) {
+                console.log(`retry request ${number} count`);
+                console.error(JSON.stringify(err));
+                retry(err);
+            });
+        }, retryOptions);
+
     }
-    put(url,data={},options){
+    put(url,data={},options,retryOptions){
         options = _.extend({body: data, json: true, simple: false, resolveWithFullResponse: true},options);
         let that = this;
         if(this.log){
             var timeLog = new TimeLogger(`PUT request URL ${url}`);
             timeLog.startTime(); console.log(`body:\n${JSON.stringify(options.body,null,2)}`);
         }
-        return rp.put(url,options).then(function ({statusCode,body,headers,request}) {
-            if(that.log){
-                timeLog.endTime();
-            }
-            return {statusCode,body,headers,request}
-        })
+        // return rp.put(url,options).then(function ({statusCode,body,headers,request}) {
+        //     if(that.log){
+        //         timeLog.endTime();
+        //     }
+        //     return {statusCode,body,headers,request}
+        // })
+        return promiseRetry(function (retry, number) {
+            return rp.put(url,options).then(function ({statusCode,body,headers,request}) {
+                if(that.log){ timeLog.endTime();}
+                return {statusCode,body,headers,request}
+            }).catch(function (err) {
+                console.log(`retry request ${number} count`);
+                console.error(JSON.stringify(err));
+                retry(err);
+            });
+        }, retryOptions);
     }
-    delete(url,qs={},options){
+    delete(url,qs={},options,retryOptions){
         options = _.extend({qs: qs,json: true, simple: false, resolveWithFullResponse: true},options);
         let that = this;
         if(this.log){
             var timeLog = new TimeLogger(`DELETE request URL ${url}${_.isEmpty(options.qs)?'':'?'+querystring.stringify(options.qs)}`);
             timeLog.startTime();
         }
-        return rp.delete(url,options).then(function ({statusCode,body,headers,request}) {
-            if(that.log){
-                timeLog.endTime();
-            }
-            return {statusCode,body,headers,request}
-        })
+        // return rp.delete(url,options).then(function ({statusCode,body,headers,request}) {
+        //     if(that.log){
+        //         timeLog.endTime();
+        //     }
+        //     return {statusCode,body,headers,request}
+        // })
+        return promiseRetry(function (retry, number) {
+            return rp.delete(url,options).then(function ({statusCode,body,headers,request}) {
+                if(that.log){ timeLog.endTime();}
+                return {statusCode,body,headers,request}
+            }).catch(function (err) {
+                console.log(`retry request ${number} count`);
+                console.error(JSON.stringify(err));
+                retry(err);
+            });
+        }, retryOptions);
     }
 }
 
